fcb72c55e2224adc47e4fc47cd309def471f62c2
nightmare <arturkag2803@gmail.com>
2026-04-06 19:33:41 +0300

    Update functions.py

--- functions.py
+++ functions.py
@@ -14,15 +14,15 @@ fgl = Figlet(font="slant")  # initialization Figlet
 class Functions:
     @staticmethod
     def open_solver():  # directs to the Solver page and clears terminal
-        os.system("clear")
+        os.system("cls")
         print(fgl.renderText(TITLES[2]))
 
     @staticmethod
     def open_helper():  # directs to the Helper page and clears terminal
-        os.system("clear")
+        os.system("cls")
         print(fgl.renderText(TITLES[1]))
 
     @staticmethod
     def open_derivative():  # directs to the Derivative page and clears terminal
-        os.system("clear")
+        os.system("cls")
         print(fgl.renderText(TITLES[3]))
